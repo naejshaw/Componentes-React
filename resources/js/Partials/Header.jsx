ad6f7fbcc74cc444e4e0ccc99e63665e58aa0006
JeanFDev <jeanjfra1@gmail.com>
2024-07-09 20:50:56 -0300

    ajustes header

--- resources/js/Partials/Header.jsx
+++ resources/js/Partials/Header.jsx
@@ -13,7 +13,6 @@ export default function Header() {
                 </Link>
 
                 <div className="relative text-2xl text-[#ededed] hover:text-[#00abf0] cursor-pointer md:!hidden">
-                    <span className="animate"></span>
                     <i className="fa fa-bars"></i>
                 </div>
 
@@ -21,56 +20,54 @@ export default function Header() {
                     <div className="text-lg text-[#ededed] font-medium ml-14 transition-[.3s] hover:text-[#00abf0]">
                         <Dropdown label="Home Sections">
                             <Dropdown.Item as={Link} href={"/#about"}>About</Dropdown.Item>
-                            <Dropdown.Item as={Link} href="/#education">Education</Dropdown.Item>
-                            <Dropdown.Item as={Link} href="/#skills">Skills</Dropdown.Item>
-                            <Dropdown.Item as={Link} href="/#contact">Contact</Dropdown.Item>
+                            <Dropdown.Item as={Link} href={"/#education"}>Education</Dropdown.Item>
+                            <Dropdown.Item as={Link} href={"/#skills"}>Skills</Dropdown.Item>
+                            <Dropdown.Item as={Link} href={"/#contact"}>Contact</Dropdown.Item>
                         </Dropdown>
                     </div>
                     <div className="text-lg text-[#ededed] font-medium ml-14 transition-[.3s] hover:text-[#00abf0]">
                         <Dropdown label="Projects 1/3">
                             <Dropdown.Item>
                                 Blog:
-                                <DropdownItem as={Link} href="/blog">All</DropdownItem>
-                                <DropdownItem as={Link} href="/blog/content/1">Article 1</DropdownItem>
-                                <DropdownItem as={Link} href="/blog/content/2">Article 2</DropdownItem>
-                                <DropdownItem as={Link} href="/blog/content/3">Article 3</DropdownItem>
-                                <DropdownItem as={Link} href="/blog/content/4">Article 4</DropdownItem>
+                                <DropdownItem as={Link} href={"/blog"}>All</DropdownItem>
+                                <DropdownItem as={Link} href={"/blog/content/1"}>Article 1</DropdownItem>
+                                <DropdownItem as={Link} href={"/blog/content/2"}>Article 2</DropdownItem>
+                                <DropdownItem as={Link} href={"/blog/content/3"}>Article 3</DropdownItem>
+                                <DropdownItem as={Link} href={"/blog/content/4"}>Article 4</DropdownItem>
                             </Dropdown.Item>
-                            <Dropdown.Item as={Link} href="/booking">Booking</Dropdown.Item>
-                            <Dropdown.Item as={Link} href="/calculator">Calculator</Dropdown.Item>
+                            <Dropdown.Item as={Link} href={"/booking"}>Booking</Dropdown.Item>
+                            <Dropdown.Item as={Link} href={"/calculator"}>Calculator</Dropdown.Item>
                             <Dropdown.Item>
                                 Clone:
-                                <Dropdown.Item as={Link} href="/clone/discord">Discord</Dropdown.Item>
-                                <Dropdown.Item as={Link} href="/clone/hbo">HBO</Dropdown.Item>
-                                <Dropdown.Item as={Link} href="/clone/youtube">YouTube</Dropdown.Item>
+                                <Dropdown.Item as={Link} href={"/clone/discord"}>Discord</Dropdown.Item>
+                                <Dropdown.Item as={Link} href={"/clone/hbo"}>HBO</Dropdown.Item>
+                                <Dropdown.Item as={Link} href={"/clone/youtube"}>YouTube</Dropdown.Item>
                             </Dropdown.Item>
-                            <Dropdown.Item as={Link} href="/cms">CMS</Dropdown.Item>
-                            <Dropdown.Item as={Link} href="/crud">CRUD</Dropdown.Item>
+                            <Dropdown.Item as={Link} href={"/cms"}>CMS</Dropdown.Item>
+                            <Dropdown.Item as={Link} href={"/crud"}>CRUD</Dropdown.Item>
                         </Dropdown>
                     </div>
                     <div className="text-lg text-[#ededed] font-medium ml-14 transition-[.3s] hover:text-[#00abf0]">
                         <Dropdown label="Projects 2/3">
-                            <Dropdown.Item as={Link} href="/dashboard">Dashboard</Dropdown.Item>
-                            <Dropdown.Item as={Link} href="/delivery">Delivery</Dropdown.Item>
-                            <Dropdown.Item as={Link} href="/ecommerce">E-commerce</Dropdown.Item>
-                            <Dropdown.Item as={Link} href="/gallery">Gallery</Dropdown.Item>
-                            <Dropdown.Item as={Link} href="/playlist">Playlist</Dropdown.Item>
-                            <Dropdown.Item as={Link} href="/portfolio">Portfolio</Dropdown.Item>
+                            <Dropdown.Item as={Link} href={"/dashboard"}>Dashboard</Dropdown.Item>
+                            <Dropdown.Item as={Link} href={"/delivery"}>Delivery</Dropdown.Item>
+                            <Dropdown.Item as={Link} href={"/ecommerce"}>E-commerce</Dropdown.Item>
+                            <Dropdown.Item as={Link} href={"/gallery"}>Gallery</Dropdown.Item>
+                            <Dropdown.Item as={Link} href={"/playlist"}>Playlist</Dropdown.Item>
+                            <Dropdown.Item as={Link} href={"/portfolio"}>Portfolio</Dropdown.Item>
                         </Dropdown>
                     </div>
                     <div className="text-lg text-[#ededed] font-medium ml-14 transition-[.3s] hover:text-[#00abf0]">
                         <Dropdown label="Projects 3/3">
-                            <Dropdown.Item as={Link} href="/quiz">Quiz</Dropdown.Item>
-                            <Dropdown.Item as={Link} href="/ranking">Ranking</Dropdown.Item>
-                            <Dropdown.Item as={Link} href="/schedule">Schedule</Dropdown.Item>
-                            <Dropdown.Item as={Link} href="/siteapi">Site API</Dropdown.Item>
-                            <Dropdown.Item as={Link} href="/socialmedia">Social Media</Dropdown.Item>
-                            <Dropdown.Item as={Link} href="/tictactoe">Tic Tac Toe</Dropdown.Item>
-                            <Dropdown.Item as={Link} href="/weather">Weather</Dropdown.Item>
+                            <Dropdown.Item as={Link} href={"/quiz"}>Quiz</Dropdown.Item>
+                            <Dropdown.Item as={Link} href={"/ranking"}>Ranking</Dropdown.Item>
+                            <Dropdown.Item as={Link} href={"/schedule"}>Schedule</Dropdown.Item>
+                            <Dropdown.Item as={Link} href={"/siteapi"}>Site API</Dropdown.Item>
+                            <Dropdown.Item as={Link} href={"/socialmedia"}>Social Media</Dropdown.Item>
+                            <Dropdown.Item as={Link} href={"/tictactoe"}>Tic Tac Toe</Dropdown.Item>
+                            <Dropdown.Item as={Link} href={"/weather"}>Weather</Dropdown.Item>
                         </Dropdown>
                     </div>
-                    <span className="active-nav"></span>
-                    <span className="animate"></span>
                 </nav>
             </header>
         </>
