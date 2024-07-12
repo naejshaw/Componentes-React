0cd08345f914becbb68bc857b3c9443983cd5d44
JeanFDev <jeanjfra1@gmail.com>
2024-07-11 23:04:53 -0300

    update gallery

--- resources/js/Pages/Projects/Gallery/Home.jsx
+++ resources/js/Pages/Projects/Gallery/Home.jsx
@@ -1,3 +1,4 @@
+import GalleryCard from "@/Components/MyComponents/GalleryCard";
 import Site from "@/Layouts/SiteLayout";
 import { Head, Link } from "@inertiajs/react";
 
@@ -7,62 +8,15 @@ export default function Home()
         <Head title="Gallery" />
         <Site>
             <section className="min-h-screen py-40 px-8 portfolio" id="portfolio">
-                <h2 className="mb-16 text-center text-7xl heading">Latest <span className="text-[#0ef]">Project</span></h2>
+                <h2 className="mb-16 text-center text-4xl md:text-5xl lg:text-7xl heading">Latest <span className="text-[#0ef]">Project</span></h2>
 
-                <div className="grid grid-cols-3 items-center gap-10 portfolio-container">
-                    <div className="relative rounded-3xl overflow-hidden flex portfolio-box">
-                        <img className="w-full ease-linear duration-500 hover:scale-110" src="https://raw.githubusercontent.com/naejshaw/Portfolio-5/main/images/portfolio1.jpg" alt=""/>
-                        <div className="absolute bottom-0 left-0 w-full h-full flex justify-center items-center flex-col text-center py-0 px-16 translate-y-48 duration-500 hover:translate-y-0 bg-[#00eeff1f] portfolio-layer">
-                            <h4 className="text-5xl">Web Design</h4>
-                            <p className="text-2xl my-1 mx-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, eaque!</p>
-                            <Link className="inline-flex justify-center items-center w-12 h-12 bg-[#fff] rounded-full" href="#"><i className="text-[#081b29] fa fa-arrow-right"></i></Link>
-                        </div>
-                    </div>
-
-                    <div className="relative rounded-3xl overflow-hidden flex portfolio-box">
-                        <img className="w-full ease-linear duration-500 hover:scale-110" src="https://raw.githubusercontent.com/naejshaw/Portfolio-5/main/images/portfolio2.jpg" alt=""/>
-                        <div className="absolute bottom-0 left-0 w-full h-full flex justify-center items-center flex-col text-center py-0 px-16 translate-y-48 duration-500 hover:translate-y-0 bg-[#00eeff1f] portfolio-layer">
-                            <h4 className="text-5xl">Web Design</h4>
-                            <p className="text-2xl my-1 mx-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, eaque!</p>
-                            <Link className="inline-flex justify-center items-center w-12 h-12 bg-[#fff] rounded-full" href="#"><i className="text-[#081b29] fa fa-arrow-right"></i></Link>
-                        </div>
-                    </div>
-
-                    <div className="relative rounded-3xl overflow-hidden flex portfolio-box">
-                        <img className="w-full ease-linear duration-500 hover:scale-110" src="https://raw.githubusercontent.com/naejshaw/Portfolio-5/main/images/portfolio3.jpg" alt=""/>
-                        <div className="absolute bottom-0 left-0 w-full h-full flex justify-center items-center flex-col text-center py-0 px-16 translate-y-48 duration-500 hover:translate-y-0 bg-[#00eeff1f] portfolio-layer">
-                            <h4 className="text-5xl">Web Design</h4>
-                            <p className="text-2xl my-1 mx-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, eaque!</p>
-                            <Link className="inline-flex justify-center items-center w-12 h-12 bg-[#fff] rounded-full" href="#"><i className="text-[#081b29] fa fa-arrow-right"></i></Link>
-                        </div>
-                    </div>
-
-                    <div className="relative rounded-3xl overflow-hidden flex portfolio-box">
-                        <img className="w-full ease-linear duration-500 hover:scale-110" src="https://raw.githubusercontent.com/naejshaw/Portfolio-5/main/images/portfolio4.jpg" alt=""/>
-                        <div className="absolute bottom-0 left-0 w-full h-full flex justify-center items-center flex-col text-center py-0 px-16 translate-y-48 duration-500 hover:translate-y-0 bg-[#00eeff1f] portfolio-layer">
-                            <h4 className="text-5xl">Web Design</h4>
-                            <p className="text-2xl my-1 mx-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, eaque!</p>
-                            <Link className="inline-flex justify-center items-center w-12 h-12 bg-[#fff] rounded-full" href="#"><i className="text-[#081b29] fa fa-arrow-right"></i></Link>
-                        </div>
-                    </div>
-
-                    <div className="relative rounded-3xl overflow-hidden flex portfolio-box">
-                        <img className="w-full ease-linear duration-500 hover:scale-110" src="https://raw.githubusercontent.com/naejshaw/Portfolio-5/main/images/portfolio5.jpg" alt=""/>
-                        <div className="absolute bottom-0 left-0 w-full h-full flex justify-center items-center flex-col text-center py-0 px-16 translate-y-48 duration-500 hover:translate-y-0 bg-[#00eeff1f] portfolio-layer">
-                            <h4 className="text-5xl">Web Design</h4>
-                            <p className="text-2xl my-1 mx-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, eaque!</p>
-                            <Link className="inline-flex justify-center items-center w-12 h-12 bg-[#fff] rounded-full" href="#"><i className="text-[#081b29] fa fa-arrow-right"></i></Link>
-                        </div>
-                    </div>
-
-                    <div className="relative rounded-3xl overflow-hidden flex portfolio-box">
-                        <img className="w-full ease-linear duration-500 hover:scale-110" src="https://raw.githubusercontent.com/naejshaw/Portfolio-5/main/images/portfolio6.jpg" alt=""/>
-                        <div className="absolute bottom-0 left-0 w-full h-full flex justify-center items-center flex-col text-center py-0 px-16 translate-y-48 duration-500 hover:translate-y-0 bg-[#00eeff1f] portfolio-layer">
-                            <h4 className="text-5xl">Web Design</h4>
-                            <p className="text-2xl my-1 mx-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, eaque!</p>
-                            <Link className="inline-flex justify-center items-center w-12 h-12 bg-[#fff] rounded-full" href="#"><i className="text-[#081b29] fa fa-arrow-right"></i></Link>
-                        </div>
-                    </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center gap-10 portfolio-container">
+                    <GalleryCard imgPath={"portfolio1"} path={"#"} text={"Descrição do projeto de Portfolio número 1"} title={"Portfolio 1"}/>
+                    <GalleryCard imgPath={"portfolio2"} path={"#"} text={"Descrição do projeto de Portfolio número 2"} title={"Portfolio 2"}/>
+                    <GalleryCard imgPath={"portfolio3"} path={"#"} text={"Descrição do projeto de Portfolio número 3"} title={"Portfolio 3"}/>
+                    <GalleryCard imgPath={"portfolio4"} path={"#"} text={"Descrição do projeto de Portfolio número 4"} title={"Portfolio 4"}/>
+                    <GalleryCard imgPath={"portfolio5"} path={"#"} text={"Descrição do projeto de Portfolio número 5"} title={"Portfolio 5"}/>
+                    <GalleryCard imgPath={"portfolio6"} path={"#"} text={"Descrição do projeto de Portfolio número 6"} title={"Portfolio 6"}/>
                 </div>
             </section>
         </Site>
