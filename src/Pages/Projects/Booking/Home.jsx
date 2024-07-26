25a43db37fbcdcedc705244bba1db3d602266c05
Jean <jeanjfra@gmail.com>
2024-07-26 17:41:41 -0300

    update nav

--- src/Pages/Projects/Booking/Home.jsx
+++ src/Pages/Projects/Booking/Home.jsx
@@ -12,8 +12,15 @@ export default function Booking()
                     <img src="https://img.freepik.com/vetores-gratis/logotipo-detalhado-da-viagem_23-2148616611.jpg?w=740&t=st=1722001140~exp=1722001740~hmac=af528988d0faae8dfaa07fdaee51bf0702cda154c7f6767614fa9063c15f2c4e" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
                     <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
                 </Navbar.Brand>
-                <Navbar.Toggle />
-                <Navbar.Collapse hidden>
+                <Navbar.Toggle className="md:hidden"/>
+                <div className="hidden md:flex">
+                    <Navbar.Link>Sobre Nós</Navbar.Link>
+                    <Navbar.Link>Acomodações</Navbar.Link>
+                    <Navbar.Link>Passagens Aéreas</Navbar.Link>
+                    <Navbar.Link>Pacotes</Navbar.Link>
+                    <Navbar.Link>Contato</Navbar.Link>
+                </div>
+                <Navbar.Collapse className="md:hidden">
                     <Navbar.Link>Sobre Nós</Navbar.Link>
                     <Navbar.Link>Acomodações</Navbar.Link>
                     <Navbar.Link>Passagens Aéreas</Navbar.Link>
