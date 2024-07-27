a5fb4ce743dbeffb8e8a8910ed1f6ec97fdebee7
Jean <jeanjfra@gmail.com>
2024-07-27 19:46:39 -0300

    update test material tailwind react

--- src/Pages/Projects/Booking/Home.jsx
+++ src/Pages/Projects/Booking/Home.jsx
@@ -1,3 +1,4 @@
+import { Carousel } from "@material-tailwind/react";
 import Site from "../../../Layouts/SiteLayout";
 import {Accordion, Datepicker, Navbar} from 'flowbite-react';
 import { Link } from "react-router-dom";
@@ -40,41 +41,24 @@ export default function Booking()
             </main>
         </div>
         <div id="content" className="mt-4 py-6 px-4 h-screen w-full">
-            <div id="animation-carousel" class="relative w-full" data-carousel="static">
-                <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-                    <div class="hidden duration-200 ease-linear" data-carousel-item>
-                        <img src="https://raw.githubusercontent.com/naejshaw/Portfolio-5/main/images/portfolio1" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                    </div>
-                    <div class="hidden duration-200 ease-linear" data-carousel-item>
-                        <img src="https://raw.githubusercontent.com/naejshaw/Portfolio-5/main/images/portfolio2" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                    </div>
-                    <div class="hidden duration-200 ease-linear" data-carousel-item="active">
-                        <img src="https://raw.githubusercontent.com/naejshaw/Portfolio-5/main/images/portfolio3" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                    </div>
-                    <div class="hidden duration-200 ease-linear" data-carousel-item>
-                        <img src="https://raw.githubusercontent.com/naejshaw/Portfolio-5/main/images/portfolio4" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                    </div>
-                    <div class="hidden duration-200 ease-linear" data-carousel-item>
-                        <img src="https://raw.githubusercontent.com/naejshaw/Portfolio-5/main/images/portfolio5" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                    </div>
-                </div>
-                <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                        <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-                        </svg>
-                        <span class="sr-only">Previous</span>
-                    </span>
-                </button>
-                <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                        <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-                        </svg>
-                        <span class="sr-only">Next</span>
-                    </span>
-                </button>
-            </div>
+            <Carousel className="rounded-xl">
+                <img
+                    src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+                    alt="imagem 1"
+                    className="h-full w-full object-cover"
+                />
+                <img
+                    src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+                    alt="imagem 2"
+                    className="h-full w-full object-cover"
+                />
+                <img
+                    src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
+                    alt="imagem 3"
+                    className="h-full w-full object-cover"
+                />
+            </Carousel>
+
         </div>
         <div id="footer" className="mt-4 py-6 px-4 h-screen w-full">
             <Accordion collapseAll>
