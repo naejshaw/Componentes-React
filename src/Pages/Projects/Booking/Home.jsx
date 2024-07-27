aa005bb7e1be2d85af29029ae78191b4e19a5f71
Jean <jeanjfra@gmail.com>
2024-07-27 12:58:28 -0300

    test carousel html

--- src/Pages/Projects/Booking/Home.jsx
+++ src/Pages/Projects/Booking/Home.jsx
@@ -1,5 +1,5 @@
 import Site from "../../../Layouts/SiteLayout";
-import {Accordion, Carousel, Datepicker, Navbar} from 'flowbite-react';
+import {Accordion, Datepicker, Navbar} from 'flowbite-react';
 import { Link } from "react-router-dom";
 export default function Booking()
 {
@@ -40,24 +40,40 @@ export default function Booking()
             </main>
         </div>
         <div id="content" className="mt-4 py-6 px-4 h-screen w-full">
-            <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-                <Carousel>
-                    <Carousel.Item>
-                        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
-                    </Carousel.Item>
-                </Carousel>
+            <div id="animation-carousel" class="relative w-full" data-carousel="static">
+                <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+                    <div class="hidden duration-200 ease-linear" data-carousel-item>
+                        <img src="/docs/images/carousel/carousel-1.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+                    </div>
+                    <div class="hidden duration-200 ease-linear" data-carousel-item>
+                        <img src="/docs/images/carousel/carousel-2.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+                    </div>
+                    <div class="hidden duration-200 ease-linear" data-carousel-item="active">
+                        <img src="/docs/images/carousel/carousel-3.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+                    </div>
+                    <div class="hidden duration-200 ease-linear" data-carousel-item>
+                        <img src="/docs/images/carousel/carousel-4.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+                    </div>
+                    <div class="hidden duration-200 ease-linear" data-carousel-item>
+                        <img src="/docs/images/carousel/carousel-5.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+                    </div>
+                </div>
+                <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                        <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+                        </svg>
+                        <span class="sr-only">Previous</span>
+                    </span>
+                </button>
+                <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                        <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                        </svg>
+                        <span class="sr-only">Next</span>
+                    </span>
+                </button>
             </div>
         </div>
         <div id="footer" className="mt-4 py-6 px-4 h-screen w-full">
