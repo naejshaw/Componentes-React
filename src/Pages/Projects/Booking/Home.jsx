87fbac128c119d078cda2aa1efefb492a9178c65
Jean <jeanjfra@gmail.com>
2024-07-29 14:48:04 -0300

    update assets

--- src/Pages/Projects/Booking/Home.jsx
+++ src/Pages/Projects/Booking/Home.jsx
@@ -125,11 +125,11 @@ export default function Booking()
             </div>
             <div id="content" className="relative mt-4 py-6 px-2 mx-auto flex justify-center h-screen w-1/2">
                 <Carousel>
-                    <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-                    <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-                    <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-                    <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-                    <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+                    <img src="../../../assets/carousel-1.svg" alt="..." />
+                    <img src="../../../assets/carousel-2.svg" alt="..." />
+                    <img src="../../../assets/carousel-3.svg" alt="..." />
+                    <img src="../../../assets/carousel-4.svg" alt="..." />
+                    <img src="../../../assets/carousel-5.svg" alt="..." />
                 </Carousel>
             </div>
             <div id="footer" className="relative mt-4 py-6 px-4 w-full">
