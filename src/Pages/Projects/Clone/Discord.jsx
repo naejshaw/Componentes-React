42705d33177b25ba9d9e9a991265acfc845af88e
Jean <jeanjfra@gmail.com>
2024-07-29 17:22:12 -0300

    update assets discord

--- src/Pages/Projects/Clone/Discord.jsx
+++ src/Pages/Projects/Clone/Discord.jsx
@@ -1,7 +1,10 @@
 import Button from "../../../Components/MyComponents/Button";
 import DiscordSection from "../../../Components/MyComponents/DiscordSection";
 import Site from "../../../Layouts/SiteLayout";
-
+import bg from '../../../assets/HBg.png';
+import bgDesk from '../../../assets/desk-HBg.png';
+import logoDesk from '../../../assets/svgexport-15-1.png';
+import logo from '../../../assets/svgexport-15.png';
 export default function Discord()
 {
     return(<>
@@ -9,9 +12,9 @@ export default function Discord()
             <div className="mt-24 pb-0 box-border flex flex-col bg-white items-center justify-center font-sans">
                 <header className="py-2 w-full flex flex-col text-white items-center justify-evenly">
                     <picture className="relative cover h-screen w-full">
-                        <source className="relative cover h-screen w-full" media="(min-width: 429px)" srcSet="https://naejshaw.github.io/discord-clone/assets/img/desk-HBg.png"/>
-                        <source className="relative cover h-screen w-full" srcSet="https://naejshaw.github.io/discord-clone/assets/img/HBg.png"/>
-                        <img className="relative cover h-screen w-full" src="https://naejshaw.github.io/discord-clone/assets/img/HBg.png" alt=""/>
+                        <source className="relative cover h-screen w-full" media="(min-width: 429px)" srcSet={bgDesk}/>
+                        <source className="relative cover h-screen w-full" srcSet={bg}/>
+                        <img className="relative cover h-screen w-full" src={bg} alt=""/>
                     </picture>
                     <h1 className="font-luckiestGuy absolute top-28 mt-20 text-xl md:text-3xl font-extrabold uppercase">Imagine um lugar...</h1>
                     <p className="absolute w-3/4 my-auto text-sm md:text-base text-wrap font-normal text-center">…onde você possa pertencer a um clube escolar, um grupo de gamers, ou uma comunidade artística mundial. Onde você e alguns amigos possam passar um tempo juntos. Um lugar que torna fácil conversar todos os dias e socializar com mais frequência.</p>
@@ -26,9 +29,9 @@ export default function Discord()
                 <DiscordSection className={"flex flex-col-reverse gap-2 justify-center items-center bg-[#f6f6f6] py-10 w-full px-4"} content={"Voz e vídeo de baixa latência, para você conversar como se estivesse na mesma sala. Dê um joinha por vídeo, veja amigos transmitirem a jogatina do dia ou junte uma galera pra desenhar na tela compartilhada."} contentClass={"text-sm md:text-base font-normal text-center text-[#23272A]"} id={"4"} title={"tecnologia de conexão confiável"} titleClass={"font-luckiestGuy text-xl md:text-3xl font-extrabold uppercase text-[#23272A] text-center"}/>
                 <footer className="bg-[#23272A] py-5 px-10 flex justify-center w-full">
                     <picture className="max-w-full block">
-                        <source media="(min-width: 429px)" srcSet="https://naejshaw.github.io/discord-clone/assets/img/svgexport-15-1.png"/>
-                        <source srcSet="https://naejshaw.github.io/discord-clone/assets/img/svgexport-15.png"/>
-                        <img src="https://naejshaw.github.io/discord-clone/assets/img/svgexport-15.png" alt=""/>
+                        <source media="(min-width: 429px)" srcSet={logoDesk}/>
+                        <source srcSet={logo}/>
+                        <img src={logo} alt=""/>
                     </picture>
                 </footer>
             </div>
