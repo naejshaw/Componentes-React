ef159680b2b0513ea771d0ed3820ee9833363856
Jean Felipe <jeanjfra@gmail.com>
2024-08-02 13:16:18 -0300

    update project

--- src/Pages/Projects/Playlist/Home.jsx
+++ src/Pages/Projects/Playlist/Home.jsx
@@ -1,112 +1,23 @@
-import { useEffect, useRef, useState } from "react";
 import Site from "../../../Layouts/SiteLayout";
-import api from '../services/api'; 
-import ReactPlayer from "react-player";
 
 export default function Playlist()
 {
 //TODO: Pesquisar e estruturar
-    const [playlists, setPlaylists] = useState([]);
-    const [currentPlaylist, setCurrentPlaylist] = useState(null);
-    const [searchResults, setSearchResults] = useState([]);
-
-    const handleSearch = async (query) => {
-        try {
-        const response = await api.search(query);
-        setSearchResults(response.data);
-        } catch (error) {
-        console.error('Erro na busca:', error);
-        }
-    };
-
-    const handlePlaylistSelect = (playlist) => {
-        setCurrentPlaylist(playlist);
-    };
-
-    useEffect(() => {
-        // Carregar playlists salvas (se houver)
-        const savedPlaylists = localStorage.getItem('playlists');
-        if (savedPlaylists) {
-        setPlaylists(JSON.parse(savedPlaylists));
-        }
-    }, []);
-
-    return(<>
-        <Site>
-            <div id="container" className="mt-24 p-8 flex items-center justify-start">
-                <SearchBar onSearch={handleSearch} />
-                <PlaylistG
-                    playlists={searchResults.length > 0 ? searchResults : playlists}
-                    onSelect={handlePlaylistSelect}
-                />
-                {currentPlaylist && <Player playlist={currentPlaylist} />}
+   const api = fetch()
+    return (<>
+      <Site>
+        <div className="container mt-12 min-h-screen py-20 px-[9%] flex items-center">
+          <div id="player" className="flex flex-col w-10/12 h-96 items-center justify-between p-4 mx-auto border border-black">
+            <div id="thumb" className="p-4 w-full h-5/6 border border-white">
+              <img src ="" alt="" className="w-full h-full"/>
             </div>
-        </Site>
-    </>);
-}
-
-function SearchBar({ onSearch }) {
-    const [query, setQuery] = useState('');
-  
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-      const results = await api.search(query);
-      onSearch(results);
-    };
-  
-    // ...
-  }
-
-  function PlaylistG({ playlist }) {
-    return (
-      <div>
-        <h2>{playlist.name}</h2>
-        <ul>
-          {playlist.items.map((item) => (
-            <li key={item.id}>{item.title}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-
-  function Player({ playlist }) {
-    const [playing, setPlaying] = useState(false);
-    const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
-    const playerRef = useRef(null);
-  
-    const handlePlayPause = () => {
-      setPlaying(!playing);
-    };
-  
-    const handleNext = () => {
-      setCurrentTrackIndex((prevIndex) => (prevIndex + 1) % playlist.length);
-    };
-  
-    const handlePrev = () => {
-      setCurrentTrackIndex((prevIndex) => (prevIndex - 1 + playlist.length) % playlist.length);
-    };
-  
-    const handleProgress = (progress) => {
-      // Atualiza a barra de progresso
-      // ...
-    };
-  
-    return (
-      <div>
-        <ReactPlayer
-          ref={playerRef}
-          url={playlist[currentTrackIndex].url}
-          playing={playing}
-          controls
-          onProgress={handleProgress}
-          onEnded={handleNext}
-        />
-        <div>
-          <button onClick={handlePlayPause}>{playing ? 'Pause' : 'Play'}</button>
-          <button onClick={handlePrev}>Previous</button>
-          <button onClick={handleNext}>Next</button>
+            <div id="controls" className="flex gap-4">
+              <div id="previous"><i className="fa fa-arrow-circle-left cursor-pointer scale-150" aria-hidden="true"></i></div>
+              <div id="playpause"><i class="fa fa-pause-circle cursor-pointer scale-150" aria-hidden="true"></i></div>
+              <div id="next"><i className="fa fa-arrow-circle-right cursor-pointer scale-150" aria-hidden="true"></i></div>
+            </div>
+          </div>
         </div>
-      </div>
-    );
+      </Site>
+    </>);
   }
