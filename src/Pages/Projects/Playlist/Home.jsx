ad931cd561ab7ff7e3f0ee26bf567edbf6d1f0f9
Jean Felipe <jeanjfra@gmail.com>
2024-08-04 12:56:57 -0300

    "Updated menu items in Playlist/Home.jsx to use h5 headings and added flex classes for layout"

--- src/Pages/Projects/Playlist/Home.jsx
+++ src/Pages/Projects/Playlist/Home.jsx
@@ -10,13 +10,13 @@ export default function Playlist()
           <div id="container" className="flex">
             <aside className="flex flex-col px-2 gap-1 min-h-96 w-1/6">
               <div id="menu" className="bg-gray-900 border border-transparent rounded-2xl p-4 h-fit">
-                <div>
+                <div className="flex w-fit justify-between">
                   <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/></svg>
-                  Home
+                  <h5>Home</h5>
                 </div>
-                <div>
+                <div className="flex w-fit justify-between">
                   <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>
-                  Search
+                  <h5>Search</h5>
                 </div>
               </div>
               <div id="Library" className="bg-gray-900 border border-transparent rounded-2xl p-2 w-full h-fit">
