afaef7e63977e4000d236dd498d31a69d1392a2e
Jean Felipe <jeanjfra@gmail.com>
2024-08-04 10:09:00 -0300

    "Added navigation elements to Library section in Playlist Home page"

--- src/Pages/Projects/Playlist/Home.jsx
+++ src/Pages/Projects/Playlist/Home.jsx
@@ -12,6 +12,16 @@ export default function Playlist()
                 <div>Search</div>
               </div>
               <div id="Library" className="bg-gray-900 border border-transparent rounded-2xl p-2 w-full h-5/6">
+                  <div className="flex justify-between">
+                    <nav className="flex justify-between gap-1 flex-wrap">
+                      <i className="fas fa-books    "></i>
+                      <button>Your Library</button>
+                    </nav>
+                    <nav className="flex justify-between list-none">
+                      <li>Create</li>
+                      <li>Show More</li>
+                    </nav>
+                  </div>
                   <div className="flex flex-wrap justify-between">
                     <nav className="flex justify-between gap-1 flex-wrap">
                       <button>Playlists</button>
