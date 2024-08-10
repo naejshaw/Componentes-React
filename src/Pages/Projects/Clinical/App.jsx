b98686b17b02568384479dca4dabe1df430bf068
Jean Felipe <jeanjfra@gmail.com>
2024-08-10 09:02:11 -0300

    "Removed react-router-dom and simplified App component to only render PatientList"

--- src/Pages/Projects/Clinical/App.jsx
+++ src/Pages/Projects/Clinical/App.jsx
@@ -1,22 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import PatientList from './components/PatientList';
-import PatientDetail from './components/PatientDetail';
-import AppointmentSchedule from './components/AppointmentSchedule';
-import MedicalRecord from './components/MedicalRecord';
-import BillingInsurance from './components/BillingInsurance';
+import PatientList from './PatientList';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={PatientList} />
-        <Route path="/patients/:id" component={PatientDetail} />
-        <Route path="/appointments" component={AppointmentSchedule} />
-        <Route path="/medical-records" component={MedicalRecord} />
-        <Route path="/billing-insurance" component={BillingInsurance} />
-      </Switch>
-    </BrowserRouter>
+    <PatientList />
   );
 };
 
